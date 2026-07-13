c7bc94a2a67279acd1d39c899a1fce60b43d9b8a
mavericks0418 <1441491421@qq.com>
2026-07-13 17:27:48 +0800

    config: configure GitHub Pages deployment

--- src/config/siteConfig.ts
+++ src/config/siteConfig.ts
@@ -12,7 +12,7 @@ export const siteConfig: SiteConfig = {
 	subtitle: "Demo site",
 
 	// 站点 URL
-	site_url: "https://firefly.cuteleaf.cn",
+	site_url: "https://mavericks0418.github.io",
 
 	// 站点描述
 	description:
